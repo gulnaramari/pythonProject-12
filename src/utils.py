ee20c2ddb7e4096ca325537d3950cefd1d548c14
gulnaramari <gulnaramari@yandex.ru>
2024-09-13 12:43:30 +0300

    получение данных по каждой карте

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,13 @@
 import datetime
 import logging
-import os
+
 import pandas as pd
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
-API_KEY_CUR = os.getenv("API_KEY_CUR")
 
-SP_500_API_KEY = os.getenv("SP_500_API_KEY")
 
 logger = logging.getLogger("utils.log")
 file_handler = logging.FileHandler("utils.log", "w")
@@ -36,4 +34,39 @@ def fetch_user_data(path_file: str) -> list[dict]:
         axis=1,
     ).tolist()
     return result
+
+
+def greeting_twenty_four_hours():
+    """Приветствие"""
+    time_obj = datetime.datetime.now()
+    if 6 <= time_obj.hour <= 12:
+        return "Доброе утро"
+    elif 13 <= time_obj.hour <= 18:
+        return "Добрый день"
+    elif 19 <= time_obj.hour <= 23:
+        return "Добрый вечер"
+    else:
+        return "Доброй ночи"
+
+def analyze_dict_user_card(my_list: list) -> list:
+    """Создает словарь по каждой карте"""
+    logger.info("Начало работы")
+    cards = {}
+    result = []
+    logger.info("Работа с операциями")
+    for i in my_list:
+        if i["Номер карты"] == "nan" or type(i["Номер карты"]) is float:
+            continue
+        elif i["Сумма платежа"] == "nan":
+            continue
+        else:
+            if i["Номер карты"][1:] in cards:
+                cards[i["Номер карты"][1:]] += float(str(i["Сумма платежа"])[1:])
+            else:
+                cards[i["Номер карты"][1:]] = float(str(i["Сумма платежа"])[1:])
+    for k, v in cards.items():
+        result.append({"last_digits": k, "total_spent": round(v, 2), "cashback": round(v / 100, 2)})
+    logger.info("Окончание работы")
+    return result
+
 
