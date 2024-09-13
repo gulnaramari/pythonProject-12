c612758df5462a3e753be9d7fb08b6d179ab64e1
gulnaramari <gulnaramari@yandex.ru>
2024-09-13 16:09:36 +0300

    создание словаря по каждой карте пользователя

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,24 @@ def greeting_twenty_four_hours():
         return "Добрый вечер"
     else:
         return "Доброй ночи"
+
+def analyze_dict_user_card(my_list: list) -> list:
+    """Создает словарь по каждой карте"""
+
+    cards = {}
+    result = []
+
+    for i in my_list:
+        if i["Номер карты"] == "nan" or type(i["Номер карты"]) is float:
+            continue
+        elif i["Сумма платежа"] == "nan":
+            continue
+        else:
+            if i["Номер карты"][1:] in cards:
+                cards[i["Номер карты"][1:]] += float(str(i["Сумма платежа"])[1:])
+            else:
+                cards[i["Номер карты"][1:]] = float(str(i["Сумма платежа"])[1:])
+    for k, v in cards.items():
+        result.append({"last_digits": k, "total_spent": round(v, 2), "cashback": round(v / 100, 2)})
+
+    return result
