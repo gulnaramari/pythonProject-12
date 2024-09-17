9b533f10d50b4e4ff2eab747cdf1f22089b2a61a
gulnaramari <gulnaramari@yandex.ru>
2024-09-17 18:51:24 +0300

    работа над main

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import os
 
 PATH_TO_XLSX = os.path.join(os.path.dirname(__file__), 'data', 'operations.xlsx')
+
--- src/utils.py
+++ src/utils.py
@@ -10,12 +10,16 @@ import urllib.request
 
 load_dotenv(".env")
 
-
 # Проверка загрузки API_KEY
 api_key = os.getenv("API_KEY")
 print(" ")
 
-SP_500_API_KEY = os.getenv("SP_500_API_KEY")
+# Проверка загрузки API_KEY_C
+API_KEY_C = os.getenv("API_KEY_C")
+print("")
+
+API_KEY_SP = os.getenv("API_KEY_SP")
+print(" ")
 
 logger = logging.getLogger("utils.log")
 logging.basicConfig(
@@ -47,15 +51,17 @@ def fetch_user_data(path_file: str) -> list[dict]:
 
 def greeting_twenty_four_hours():
     """Приветствие"""
-    time_obj = datetime.datetime.now()
-    if 6 <= time_obj.hour <= 12:
-        return "Доброе утро"
-    elif 13 <= time_obj.hour <= 18:
-        return "Добрый день"
-    elif 19 <= time_obj.hour <= 23:
-        return "Добрый вечер"
+    dict_all = {"greeting": ("доброе утро", "добрый день", "добрый вечер", "доброй ночи")}
+    current_time = datetime.datetime.now()
+    if current_time.hour >= 4 and current_time.hour <= 12:
+        greet = dict_all["greeting"][0]
+    elif current_time.hour >= 12 and current_time.hour <= 16:
+        greet = dict_all["greeting"][1]
+    elif current_time.hour >= 16 and current_time.hour <= 24:
+        greet = dict_all["greeting"][2]
     else:
-        return "Доброй ночи"
+        greet = dict_all["greeting"][3]
+    return greet
 
 
 def analyze_dict_user_card(all_data: list) -> list:
@@ -85,51 +91,68 @@ def analyze_dict_user_card(all_data: list) -> list:
     return result
 
 
+def fetch_currency_rates_values(currency: list) -> list[dict]:
+    """Функция запроса курса валют"""
+    logger.info("Начало работы функции (currency_rates)")
+    api_key = API_KEY_C
+    result = []
+    for i in currency:
+        url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{i}"
+        with urllib.request.urlopen(url) as response:
+            body_json = response.read()
+        body_dict = json.loads(body_json)
+        result.append({"currency": i, "rate": round(body_dict["conversion_rates"]["RUB"], 2)})
+
+    logger.info("Создание списка словарей для функции - currency_rates")
+
+    logger.info("Окончание работы функции - currency_rates")
+    return result
 
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def top_user_transactions(my_list: list) -> list:
+    """Функция для получения топ-5 транзакций по сумме платежа"""
+    logger.info("Начало работы функции (top_five_transaction)")
+    all_transactions = {}
+    result = []
+    logger.info("Перебор транзакций в функции (top_five_transaction)")
+    for i in my_list:
+        if i["Категория"] not in all_transactions and str(i["Сумма платежа"])[0:1] != "-":
+            if i["Категория"] != "Пополнения":
+                all_transactions[i["Категория"]] = float(str(i["Сумма платежа"])[1:])
+        elif (
+                i["Категория"] in all_transactions
+                and float(str(i["Сумма платежа"])[1:]) > all_transactions[i["Категория"]]
+        ):
+            all_transactions[i["Категория"]] = float(str(i["Сумма платежа"])[1:])
+    for i in my_list:
+        for k, v in all_transactions.items():
+            if k == i["Категория"] and v == float(str(i["Сумма платежа"])[1:]):
+                result.append({"date": i["Дата платежа"], "amount": v, "category": k, "description": i["Описание"]})
+    logger.info("Окончание работы функции (top_five_transaction)")
+
+    return result
 
 
-def fetch_currency_rates(currency: list) -> list[dict]:
-	"""Получение курса валют"""
-	logger.info("Beginning of the work...")
-	api_key = os.getenv("API_KEY")
-	for curr in currency:
-		try:
-			url = (
-				f"https://api.apilayer.com/exchangerates_data/convert?"
-				f"to=RUB&from={curr}&amount={2}"
-			)
-			headers = {"apikey": api_key}
-			response = requests.get(url, headers=headers)
-			json_result = response.json()
-			amount_rub = json_result["result"]
-			return amount_rub
+def fetch_stock_prices_values(stocks: list) -> list:
+    """Функция для получения данных об акциях из списка S&P500"""
+    logger.info("Начало работы функции (get_price_stock)")
+    api_key = API_KEY_SP
+    stock_prices = []
+    logger.info("Функция обрабатывает данные транзакций.")
+    for stock in stocks:
+        logger.info("Перебор акций в списке 'stocks' в функции (get_price_stock)")
+        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={api_key}"
+        response = requests.get(url, timeout=5, allow_redirects=False)
+        result = response.json()
 
-		except Exception as e:
-			logger.error(f"An error occurred while fetching the data for {curr}: {e}")
+        stock_prices.append({"stock": stock, "price": round(float(result["Global Quote"]["05. price"]), 2)})
+    logger.info("Функция get_price_stock успешно завершила свою работу")
+    return stock_prices
 
-	logger.info("Formation of list of dictionaries")
-	logger.info("Finish of work!")
-	return result
+
 
 
 if __name__ == "__main__":
     result = fetch_user_data("..\\data\\operations.xlsx")
     print(result)
-
-
-if __name__ == "__main__":
-    result_ = fetch_currency_rates(['USD', 'EUR', 'BDT'])
-    print(result_)
-
-
-
-
-
-
-
-
 
